e2da8e9186f1f247db7f9451ba4f7b99c8141952
rajin-siam <mdrajinmashrursiam@gmail.com>
2025-05-01 17:37:43 +0600

    feat: Implement order and payment processing system

--- src/main/java/com/ecommerce/project/model/Product.java
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -22,6 +22,7 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
+
     @NotBlank
     @Size(min = 3, message = "Product Name must contain at least 3 characters")
     private String productName;
