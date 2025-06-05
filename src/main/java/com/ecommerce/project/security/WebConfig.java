f3a111bc36e38dc482069d1c5b49eea8097bbb64
rajin-siam <mdrajinmashrursiam@gmail.com>
2025-06-06 01:11:12 +0600

    bug fix

--- src/main/java/com/ecommerce/project/security/WebConfig.java
+++ src/main/java/com/ecommerce/project/security/WebConfig.java
@@ -7,14 +7,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5174")
-                        .allowedOrigins("https://shopverse-frontend-rabt.onrender.com")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        }
-
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5174",
+                        "https://shopverse-frontend-rabt.onrender.com"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*")
+                .exposedHeaders("Set-Cookie")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
